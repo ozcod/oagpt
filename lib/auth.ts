c009e3ba31f8e4462b05cb87c8f1eaa5eb1990ac
Ozair Ahmad <ozair.ahmad@stud.uni-bamberg.de>
2026-04-05 16:37:05 +0200

    feature: Add SignIn with Google

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,11 +4,21 @@ import { db } from "@/db/index"; // your drizzle instance
 import { schema } from "@/db/schema/auth-schema";
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
   database: drizzleAdapter(db, {
     provider: "pg",
     schema: schema,
   }),
   emailAndPassword: {
     enabled: true,
+  },
+
+  socialProviders: {
+    google: {
+      accessType: "offline",
+      prompt: "select_account consent",
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
   },
 });
